d24b093dd03110f9792c1c14e444ed9dc9d796d7
ErosFerreira-A <erosferreira00@gmail.com>
2024-08-21 15:32:31 -0300

    Commit inicial

---
+++ Models/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Exemplo.Models;
+
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("João", 30);
+            p.Apresentar();
+        }
+    }
+}
---
+++ cod/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class Car 
+{
+  public void fullThrottle()
+  {
+    Console.WriteLine("The car is going as fast as it can!"); 
+  }
+
+  static void Main(string[] args)
+  {
+    Car myObj = new Car();
+    
+    myObj.fullThrottle();
+
+  }
+} 
